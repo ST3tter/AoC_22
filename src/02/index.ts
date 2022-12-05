e7dd8e5d75adf595b33434e0d2923a06eddd814a
ST3tter <st3tter@gmail.com>
2022-12-05 07:00:28 +0100

    Part 2 of day 2 done

--- src/02/index.ts
+++ src/02/index.ts
@@ -27,4 +27,31 @@ const matchArray = inputString.replaceAll(' ', '').split('\n');
 /* Calculate the sum of the matches with the combinations LUT */
 const sum = matchArray.reduce((acc, current) => acc + combinations[current], 0);
 
-console.log(sum);
+console.log('Part 1: The sum of all matches is:', sum);
+
+/* ------- PART 2 ------- */
+
+/* LUT for the match combinations
+  A Rock = 1
+  B Paper = 2
+  C Scissors = 3
+
+  X Loose = 0
+  Y Draw = 3
+  Z Win = 6 */
+const strategies: { [key: string]: number } = {
+  AX: 3 + 0,
+  AY: 1 + 3,
+  AZ: 2 + 6,
+  BX: 1 + 0,
+  BY: 2 + 3,
+  BZ: 3 + 6,
+  CX: 2 + 0,
+  CY: 3 + 3,
+  CZ: 1 + 6,
+};
+
+/* Calculate the sum of the matches with the strategies LUT */
+const Part2sum = matchArray.reduce((acc, current) => acc + strategies[current], 0);
+
+console.log('Part 2: The sum of all matches is:', Part2sum);
